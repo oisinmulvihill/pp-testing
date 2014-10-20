2648ab606887de13290fd5fa464c38083ca70e6b
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2014-10-20 17:46:44 +0100

    ditching pkglib as its causing more pain then usefulness to me.

--- setup.py
+++ setup.py
@@ -1,30 +1,72 @@
-# $HeadURL$
-try:
-    from pkglib.setuptools import setup
-except ImportError:
-    print "PkgLib is not available. Please run \"easy_install pkglib\""
-    import sys
-    sys.exit(1)
+# -*- coding: utf-8 -*-
+"""
+"""
+from setuptools import setup, find_packages
 
-# ------------------ Define your C-extensions here --------------------- #
 
-# Conventions:
-# Source code under '<package root>/src/'
-# Extension modules names begin with an underscore: eg, '_xyz'
-# to differentiate them from regular Python modules.
+Name = "pp-testing"
+ProjectUrl = ""
+Version = "1.0.0"
+Author = 'Oisin Mulvihill, Edward Easton'
+AuthorEmail = 'oisin mulvihill at gmail'
+Maintainer = ''
+Summary = 'Testing Helper Library.'
+License = ''
+Description = Summary
+ShortDescription = Summary
 
-# import numpy
-# extra_compile_args = ['-O0']
 
-# setup( ext_modules = [
-#        Extension('acme.mypackage._foo', ['src/foo1.c', 'src/foo2.c']  \
-#                   include_dirs=[ numpy.get_include() ],
-#                   extra_compile_args=extra_compile_args,
-#        ),
-#        Extension('acme.mypackage._bar', ['src/bar1.c', 'src/bar2.c']  \
-#                   include_dirs=[ numpy.get_include() ],
-#                   extra_compile_args=extra_compile_args,
-#       ),
-# ])
+needed = [
+    "pytest",
+    "pymongo",
+]
 
-setup()
+test_needed = [
+    "pytest-cov",
+]
+
+test_suite = ''
+
+EagerResources = [
+    'pp',
+]
+
+ProjectScripts = [
+]
+
+PackageData = {
+    '': ['*.*'],
+}
+
+
+# [console] ??
+#     testing = pp.testing.scripts.app:main
+EntryPoints = """
+"""
+
+setup(
+    url=ProjectUrl,
+    name=Name,
+    zip_safe=False,
+    version=Version,
+    author=Author,
+    author_email=AuthorEmail,
+    description=ShortDescription,
+    long_description=Description,
+    classifiers=[
+        "Topic :: Software Development :: Libraries",
+        "Programming Language :: Python",
+    ],
+    keywords='python',
+    license=License,
+    scripts=ProjectScripts,
+    install_requires=needed,
+    tests_require=test_needed,
+    test_suite=test_suite,
+    include_package_data=True,
+    packages=find_packages(),
+    package_data=PackageData,
+    eager_resources=EagerResources,
+    entry_points=EntryPoints,
+    namespace_packages=['pp'],
+)
